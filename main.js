d96234ba2de4ac56993103f78f9cfcb7557f92ad
oppp0999 <oppp0999@naver.com>
2022-09-30 23:18:27 +0900

    version0.3_220930_1

--- main.js
+++ main.js
@@ -7,12 +7,14 @@ var authRouter = require('./routes/auth')(passport);
 var indexRouter = require('./routes/index');
 var userRouter = require('./routes/user');
 
+app.use("/assets", express.static("assets"));
 app.use('/', indexRouter);
 app.use('/auth/', authRouter);
 app.use('/user/', userRouter);
 app.use(pathComp('lib')); //외부 css적용을 위해서 lib 폴더를 정적으로 알려주는 모듈 제일 마지막에 써야함
 //app.use(pathComp('public'));
 app.use(express.static('pulbic'));
+
 
 app.use(function(req, res, next) {//404 error 처리하는 미들웨어
     res.status(404).send('Sorry cant find that!');
